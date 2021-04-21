44bfb80a2a03af4315963438fc6aa86eea95a00f
zefanyasendri <zefanyasendri@gmail.com>
2021-04-21 14:16:23 +0700

    Inisialisasi File

---
+++ controllers/controller_administrator.go
@@ -0,0 +1,10 @@
+package controllers
+
+// import (
+// 	"encoding/json"
+// 	"log"
+// 	"fmt"
+// 	"net/http"
+// 	"strconv"
+// 	"github.com/gorilla/mux"
+// )
---
+++ models/administrator.go
@@ -0,0 +1,4 @@
+package models
+
+// type Administrator struct {}
+// type AdministratorResponse struct {}
