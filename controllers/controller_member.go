7a94e3fadb624390b0414be7469c5b44853e75ae
zefanyasendri <zefanyasendri@gmail.com>
2021-04-27 16:05:25 +0700

    fitur GetFilmByKeyword Member

--- controllers/controller_member.go
+++ controllers/controller_member.go
@@ -39,3 +39,25 @@ func UpdateProfile(w http.ResponseWriter, r *http.Request) {
 	w.Write(result)
 
 }
+
+func GetFilmByKeywords(w http.ResponseWriter, r *http.Request) {
+	db := database.ConnectDB()
+
+	vars := mux.Vars(r)
+	keywordfilm := vars["keywords"]
+
+	var film []models.Film
+
+	db.Table("films").Select("films.id_film,films.judul,films.tahun_rilis, films.sutradara, films.sinopsis, films.id_genre").Joins("LEFT JOIN genres ON films.id_genre = genres.id_genre LEFT JOIN list_pemains ON films.id_film = list_pemains.id_film LEFT JOIN pemains ON list_pemains.id_pemain = pemains.id_pemain").Where("films.judul LIKE ? OR films.sutradara LIKE ? OR films.tahun_rilis LIKE ? OR films.sinopsis LIKE ? OR genres.jenis_genre LIKE ? OR pemains.nama_pemain LIKE ?", "%"+keywordfilm+"%", "%"+keywordfilm+"%", "%"+keywordfilm+"%", "%"+keywordfilm+"%", "%"+keywordfilm+"%", "%"+keywordfilm+"%").Scan(&film)
+
+	response := models.FilmResponse{Status: 200, Data: film, Message: "Data Found"}
+	result, err := json.Marshal(response)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
