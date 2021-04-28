cdbdf92e7974f6dd381f5dcc066a5f8ec92b785e
zefanyasendri <zefanyasendri@gmail.com>
2021-04-28 19:18:38 +0700

    Minor Fix Controller Member

--- models/member.go
+++ models/member.go
@@ -1,16 +1,17 @@
 package models
 
 type Member struct {
-	Email         string  `json:"email"`
-	Password      string  `json:"password"`
-	IdMember      int     `json:"idMember" gorm:"primaryKey"`
-	NamaLengkap   string  `json:"namaLengkap"`
-	TanggalLahir  string  `json:"tanggalLahir"`
-	JenisKelamin  string  `json:"jenisKelamin"`
-	AsalNegara    string  `json:"asalNegara"`
-	StatusAkun    string  `json:"statusAkun"`
-	NoKartuKredit string  `json:"noKartuKredit" gorm:"type:varchar(191)"`
-	History       History `gorm:"foreignKey:IdMember"`
+	Email         string      `json:"email"`
+	Password      string      `json:"password"`
+	IdMember      int         `json:"idMember" gorm:"primaryKey"`
+	NamaLengkap   string      `json:"namaLengkap"`
+	TanggalLahir  string      `json:"tanggalLahir"`
+	JenisKelamin  string      `json:"jenisKelamin"`
+	AsalNegara    string      `json:"asalNegara"`
+	StatusAkun    string      `json:"statusAkun"`
+	NoKartuKredit string      `json:"noKartuKredit" gorm:"type:varchar(191)"`
+	History       History     `gorm:"foreignKey:IdMember"`
+	KartuKredit   KartuKredit `gorm:"foreignKey:NoKartuKredit"`
 }
 
 type MemberResponse struct {
